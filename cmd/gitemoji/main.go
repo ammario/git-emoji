c7db2bfc34201a968bfa23e37ca08a4c5d660137
Ammar Bandukwala <ammar@ammar.io>
2023-03-12 15:22:30 -0500

    📦 Collapse all into main command

--- cmd/gitemoji/main.go
+++ cmd/gitemoji/main.go
@@ -1,9 +1,18 @@
 package main
 
 import (
+	"bytes"
+	"fmt"
+	"io"
 	"os"
+	"os/exec"
+	"strings"
+
+	_ "embed"
 
 	"github.com/coder/flog"
+	"github.com/fatih/color"
+	"github.com/forPelevin/gomoji"
 	"github.com/sashabaranov/go-openai"
 	"github.com/spf13/cobra"
 )
@@ -17,13 +26,105 @@ func newClient() *openai.Client {
 	return openai.NewClient(key)
 }
 
-func main() {
-	cmd := &cobra.Command{
-		Use: "gitemoji",
+//go:embed prelude.txt
+var promptPrelude string
+
+func readCommitFromCwd() (string, error) {
+	var buf bytes.Buffer
+	c := exec.Command("git", "log", "--pretty=format:%s", "-1")
+	c.Stdout = &buf
+	err := c.Run()
+	if err != nil {
+		return "", err
 	}
-	cmd.AddCommand(
-		amendCmd(),
+	return buf.String(), nil
+}
+
+func amendName(s string) error {
+	c := exec.Command("git", "commit", "--amend", "-m", s)
+	err := c.Run()
+	if err != nil {
+		return err
+	}
+	return nil
+}
+
+func main() {
+	var (
+		readStdin bool
+		dryRun    bool
 	)
+	cmd := &cobra.Command{
+		Use:   "gitemoji",
+		Short: "Add an emoji to your last commit message",
+		Run: func(cmd *cobra.Command, args []string) {
+			client := newClient()
+
+			var originalCommitMessage string
+			if readStdin {
+				byt, err := io.ReadAll(os.Stdin)
+				if err != nil {
+					flog.Fatalf("read stdin: %v", err)
+				}
+				originalCommitMessage = string(byt)
+			} else {
+				var err error
+				originalCommitMessage, err = readCommitFromCwd()
+				if err != nil {
+					flog.Fatalf("read commit: %v", err)
+				}
+			}
+
+			originalCommitMessage = strings.TrimSpace(originalCommitMessage)
+
+			// Remove existing emojis to make it easy to retry.
+			originalCommitMessageNoEmoji := gomoji.RemoveEmojis(originalCommitMessage)
+
+			resp, err := client.CreateCompletion(cmd.Context(), openai.CompletionRequest{
+				Model:       "text-davinci-003",
+				MaxTokens:   10,
+				Temperature: 0.8,
+				Prompt:      promptPrelude + "\nCommit: " + gomoji.RemoveEmojis(originalCommitMessageNoEmoji),
+			})
+			if err != nil {
+				flog.Fatalf("create completion: %v", err)
+			}
+			var best string
+			for _, choice := range resp.Choices {
+				// flog.Infof("completion: %q", choice.Text)
+				emojis := gomoji.FindAll(choice.Text)
+				if len(emojis) > 0 {
+					best = emojis[0].Character
+					break
+				}
+			}
+			if best == "" {
+				color.Yellow("No emoji found, exiting\n")
+				return
+			}
+
+			newCommitMessage := best + " " + originalCommitMessageNoEmoji
+
+			if newCommitMessage == originalCommitMessage {
+				color.Yellow("No change, exiting\n")
+				return
+			}
+
+			if dryRun {
+				color.Blue("Dry run, would have amended to: \n")
+			} else {
+				color.Magenta("> git commit --amend \n")
+				err := amendName(newCommitMessage)
+				if err != nil {
+					flog.Fatalf("amend: %v", err)
+				}
+			}
+			fmt.Fprintf(cmd.OutOrStdout(), "%s\n", newCommitMessage)
+		},
+	}
+
+	cmd.Flags().BoolVar(&readStdin, "read-stdin", false, "read commit message from stdin")
+	cmd.Flags().BoolVar(&dryRun, "dry", false, "don't actually amend the commit, just print the new message")
 
 	err := cmd.Execute()
 	if err != nil {
